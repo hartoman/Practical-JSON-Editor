162ca6f678413809c8e9e4040d5f713d5545bc62
chris <you@example.com>
2024-03-27 16:50:29 +0200

    improved search by regex

--- js/functions/searchHandler.js
+++ js/functions/searchHandler.js
@@ -6,6 +6,8 @@ const selectors = {
 };
 
 let currentIndex = 0;
+let numOccurences = 0;
+let foundIn;
 
 // modal toggle enabled search btn
 $(selectors.searchInput).on("input", function () {
@@ -16,16 +18,24 @@ $(selectors.searchInput).on("input", function () {
     $("#search").button("disable");
   } else {
     const searchValue = $(selectors.searchInput).val();
+
+    let regex = new RegExp(`.*(${searchValue}).*`);
+
+    // Use the filter method to select inputs containing the regex pattern
+    const possibleInputs = $("#mainContainer").find("input");
+
+    foundIn = $(possibleInputs).filter(function () {
+      return regex.test($(this).val());
+    });
+
     // Select all input elements of type text and number that have a specific value
-    const foundIn = $('input[type=text][value="' + searchValue + '"], input[type=number][value="' + searchValue + '"]'); //
-    const numOccurences = $(foundIn).length;
+    //   const foundIn = $('input[type=text][value="' + regex + '"], input[type=number][value="' + searchValue + '"]'); //
+    numOccurences = $(foundIn).length;
     if (!numOccurences) {
       // value not found
-      $(selectors.searchModalCreateBtn).prop("disabled", true);
       $("#search").button("disable");
     } else {
       // not empty and value found
-      $(selectors.searchModalCreateBtn).prop("disabled", false);
       $("#search").button("enable");
     }
     $(selectors.timesFoundText).css("visibility", "visible");
@@ -35,24 +45,24 @@ $(selectors.searchInput).on("input", function () {
 
 export const cycleFound = () => {
   const searchValue = $(selectors.searchInput).val();
-  const instances = $('input[type=text][value="' + searchValue + '"], input[type=number][value="' + searchValue + '"]'); //;
-
-  if (instances.length) {
+  //instances = $('input[type=text][value="' + searchValue + '"], input[type=number][value="' + searchValue + '"]'); //;
+  const instances = foundIn;
+  
+  if (instances.length > 0) {
     if (currentIndex >= $(instances).length) {
       currentIndex = 0;
     }
 
     const cur = $(instances).get(currentIndex);
     const allAncestors = $(cur).parents();
-  //  const parentObjectWrapper = $(cur).parents()[2];
-  //  const previousSiblingsofParent = $(parentObjectWrapper).prevAll();
+    //  const parentObjectWrapper = $(cur).parents()[2];
+    //  const previousSiblingsofParent = $(parentObjectWrapper).prevAll();
     $(allAncestors).removeClass("d-none start-invisible");
- //   $(previousSiblingsofParent).removeClass("start-invisible");
-    
+    //   $(previousSiblingsofParent).removeClass("start-invisible");
 
     let firstNode = $(".start-invisible").first();
-    let sel = $(firstNode).nextUntil($(cur), '.start-invisible')
-    $(sel).removeClass('start-invisible')
+    let sel = $(firstNode).nextUntil($(cur), ".start-invisible");
+    $(sel).removeClass("start-invisible");
 
     cur.scrollIntoView({
       behavior: "smooth",
@@ -60,6 +70,8 @@ export const cycleFound = () => {
     });
     lazy.lazyLoad();
     currentIndex++;
+
+    $(selectors.timesFoundText).text(`${currentIndex} / ${numOccurences}`);
   }
 };
 
